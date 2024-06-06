cf07927fab810965bec98c65d59dbe6d8a709070
Shitiz Rajvanshi <86548099+Shitiz45@users.noreply.github.com>
2024-06-06 23:09:17 +0530

    set up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
+
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
